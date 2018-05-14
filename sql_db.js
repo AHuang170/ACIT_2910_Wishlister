02000910b0918a1b41534ca2efc018fbb1fb8e5c
talfreds <tyler.alfreds@gmail.com>
2018-05-14 13:55:58 -0700

    fixed module exports on sql_db

--- sql_db.js
+++ sql_db.js
@@ -450,9 +450,5 @@ var check_token = (uid, token, currentTime, tokenTime) => {
 }
 
 module.exports = {
-<<<<<<< HEAD
-  fetch_wishlist, fetch_wishlist_duplicates, insert_wishlist, fetch_user_detail, insert_user, check_user_existence, check_email_existence, delete_from_wishlist, get_uid_from_email, connection, current_uid
-=======
-  fetch_wishlist, fetch_wishlist_duplicates, insert_wishlist, fetch_user_detail, insert_user, check_user_existence, check_email_existence, delete_from_wishlist, get_uid_from_email, update_password, update_token, check_token
->>>>>>> password-reset-function
+  fetch_wishlist, fetch_wishlist_duplicates, insert_wishlist, fetch_user_detail, insert_user, check_user_existence, check_email_existence, delete_from_wishlist, get_uid_from_email, connection, current_uid, update_password, update_token, check_token
 }
